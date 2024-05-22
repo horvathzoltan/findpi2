f29970cfead989b37e4783beb38429f3cba3a79d
Horváth Zoltán <horvath.zoltan.mobil@gmail.com>
2024-05-22 15:36:55 +0200

    12

--- stringhelper.cpp
+++ stringhelper.cpp
@@ -1,8 +1,8 @@
 #include "stringhelper.h"
 
-#if QT_VERSION > QT_VERSION_CHECK(6, 0, 0)
-    const Qt::SplitBehavior StringHelper::SplitBehavior = Qt::SplitBehaviorFlags::SkipEmptyParts,
-#elif QT_VERSION > QT_VERSION_CHECK(5, 0, 0)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
+    const Qt::SplitBehavior StringHelper::SplitBehavior = Qt::SplitBehaviorFlags::SkipEmptyParts;
+#else
     const QString::SplitBehavior StringHelper::SplitBehavior = QString::SplitBehavior::SkipEmptyParts;
 #endif
 
